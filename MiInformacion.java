63b4ec935e15761958eeb73d6227207cdabb2e62
Guillermo Barrientos <guille.work.94@gmail.com>
2024-10-01 09:01:12 -0400

    Primer Commit

---
+++ MiInformacion.java
@@ -0,0 +1,16 @@
+public class MiInformacion {
+
+    public static void main(String[] args) {
+        String nombre = "Guillermo José";
+        String apellido = "Barrientos Genez";
+        int edad = 30;
+        String ciudad = "San Lorenzo";
+        String pais = "Paraguay";
+        String pasatiempo = "Soy un apasionado por la tecnología y el deporte.";
+
+        System.out.println("Hola, mi nombre es "+nombre+" y mi apellido "+apellido);
+        System.out.println("Tengo "+edad+" años de edad.");
+        System.out.println("Soy de la ciudad de "+ciudad+", "+pais);
+        System.out.println(pasatiempo);
+    }
+}
